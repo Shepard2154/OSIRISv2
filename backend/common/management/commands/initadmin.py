594ae999bdd1efe4fdc9915ef8565cf8380aa86e
john <john-shepard-2154@yandex.ru>
2022-09-09 14:28:49 +0300

    updated initadmin command

--- backend/common/management/commands/initadmin.py
+++ backend/common/management/commands/initadmin.py
@@ -1,12 +1,14 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import DoesNotExist
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         User = get_user_model()
-        if not User.objects.get(username="admin"): 
+        try:
+            User.objects.get(username="admin")
             User.objects.create_superuser('admin', 'admin@myproject.com', 'password')
-        else:
+        except DoesNotExist:
             self.stdout.write("Admin account has already existed")
